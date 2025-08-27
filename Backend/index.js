baeef555a91f7cdb1ce41efac54a7303da93d987
Aatif <aatifkhan2001@gmail.com>
2025-08-27 14:36:44 +0530

    Feat : Added index

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const port = 3000
 app.use(express.json())
  
 
-app.post('/todo',(req,res)=>{
+app.post('/todo',async(req,res)=>{
 
   const createPayload = req.body
   const parsedPayload = createTodo.safeParse(createPayload)
@@ -16,28 +16,45 @@ app.post('/todo',(req,res)=>{
     })
     return
   }
-
-
+//put it in mongodb
+  await todo.create({
+    title:createPayload.title,
+    description:createPayload.description,
+    completed:false
+  });
+  
+   res.json({
+    msg : "Todo created"
+   })
 
 })
 
 
 
-app.get('/todo',(req,res)=>{
-  console.log("Hi");
-  res.send({
-    msg:"Hi"
-  })
+app.get('/todos',async(req,res)=>{
+const todos =   await todo.find({})
+res.json({
+  todos
+})
 })
 
-app.put('/completed',(req,res)=>{
+app.put('/completed',async(req,res)=>{
 const updatedPayload = req.body
 const parsedPayload = updateTodo.safeParse(updatedPayload )
      if(!parsedPayload){
       res.status(411).json({
         msg:"you have sent wrong inputs"
       })
+      return
      }
+     await todo.update({
+      _id:req.body.id
+     },{
+      completed:true
+     })
+     res.json({
+      msg:"Todo marked as completed"
+     })
 })
 
 async function main(){
