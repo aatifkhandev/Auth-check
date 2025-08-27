871251c7e3fbba1daaca43cf6d5fd3c0911c544f
Aatif <aatifkhan2001@gmail.com>
2025-08-27 14:07:13 +0530

    Feat : Added post , put end points

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const { createTodo, updateTodo } = require('./types')
 const app = express()
 const port = 3000
 
 app.use(express.json())
-
+ 
 
 app.post('/todo',(req,res)=>{
 
+  const createPayload = req.body
+  const parsedPayload = createTodo.safeParse(createPayload)
+  if(!parsedPayload){
+    res.status(411).json({
+      msg:"you sent the wrong inputs"
+    })
+    return
+  }
+
+
+
 })
+
+
 
 app.get('/todo',(req,res)=>{
   console.log("Hi");
@@ -17,7 +31,13 @@ app.get('/todo',(req,res)=>{
 })
 
 app.put('/completed',(req,res)=>{
-
+const updatedPayload = req.body
+const parsedPayload = updateTodo.safeParse(updatedPayload )
+     if(!parsedPayload){
+      res.status(411).json({
+        msg:"you have sent wrong inputs"
+      })
+     }
 })
 
 app.listen(port,()=>{
